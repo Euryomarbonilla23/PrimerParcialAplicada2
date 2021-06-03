ccba970744c0a13841c2c42ae65e71b8db440a59
Eury Omar <Eury Omar@LAPTOP-35F7H0GD>
2021-06-03 07:35:37 -0500

    Add project files.

---
+++ PrimerParcialAplicada2/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimerParcialAplicada2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerParcialAplicada2.DAL
+{
+    public class Contexto : DbContext
+
+    {
+        public DbSet <Articulos> Articulos  { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=Data\Parcial1.db");
+        }
+    }
+}
